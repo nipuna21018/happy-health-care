215e3e1bbc1cfae1e5ffeab90daa10e5808baf4f
Nipuna Harshana <nipuna.harshana@infor.com>
2021-03-08 21:52:54 +0530

    Resources paths not found bug fix

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -283,21 +283,19 @@
 		</div>
 	</div>
 	<!-- Bootstrap core JavaScript-->
-	<script src="{{ asset('admin/jquery/jquery.min.js') }}"></script>
-	<script src="{{ asset('admin/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+	<script src="{{ asset('admin/vendor/jquery/jquery.min.js') }}"></script>
+	<script src="{{ asset('admin/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 	<!-- Core plugin JavaScript-->
-	<script src="{{ asset('admin/jquery-easing/jquery.easing.min.js') }}"></script>
+	<script src="{{ asset('admin/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
 	<!-- Page level plugin JavaScript-->
-	<script src="{{ asset('admin/chart.js/Chart.js') }}"></script>
-	<script src="{{ asset('admin/datatables/jquery.dataTables.js') }}"></script>
-	<script src="{{ asset('admin/datatables/dataTables.bootstrap4.js') }}"></script>
-	<script src="{{ asset('admin/jquery.selectbox-0.2.js') }}"></script>
-	<script src="{{ asset('admin/retina-replace.min.js') }}"></script>
-	<script src="{{ asset('admin/jquery.magnific-popup.min.js') }}"></script>
+	<script src="{{ asset('admin/vendor/datatables/jquery.dataTables.js') }}"></script>
+	<script src="{{ asset('admin/vendor/datatables/dataTables.bootstrap4.js') }}"></script>
+	<script src="{{ asset('admin/vendor/jquery.selectbox-0.2.js') }}"></script>
+	<script src="{{ asset('admin/vendor/retina-replace.min.js') }}"></script>
+	<script src="{{ asset('admin/vendor/jquery.magnific-popup.min.js') }}"></script>
 	<!-- Custom scripts for all pages-->
 	<script src="{{ asset('admin/js/admin.js') }}"></script>
-	<!-- Custom scripts for this page-->
-	<script src="{{ asset('admin/js/admin-charts.js') }}"></script>
+	
 
 	<!-- SPECIFIC SCRIPTS -->
 	@yield('scripts')
